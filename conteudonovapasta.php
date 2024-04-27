2114e37257d1639b4b502907af0d7175c5daf6c2
Gabriel Andrade Cunha <64712751+andradedevweb@users.noreply.github.com>
2024-04-26 22:44:20 -0400

    Add files via upload

---
+++ conteudonovapasta.php
@@ -0,0 +1,191 @@
+<?php
+
+//Função para deletar diretorios
+function delTree($dir) { 
+    $files = array_diff(scandir($dir), array('.','..')); 
+    foreach ($files as $file) { 
+    (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+    } 
+    return rmdir($dir); 
+}
+
+if(isset($_POST['acao'])){
+            
+    $arquivo = $_FILES['file'];
+    $nomepastaupload = basename(__FILE__);
+    @$novonomepastaupload = pathinfo($nomepastaupload, PATHINFO_FILENAME);
+    $arquivoNovo = explode('.',$arquivo['name']);
+    $path = "../../../models/$novonomepastaupload/".$arquivo['name'];
+    $arquivoNome = $arquivo['name'];
+
+    if($arquivoNovo[sizeof($arquivoNovo)-1] != 'mp3'){
+        echo "<script type='text/javascript'>alert('O arquivo não é mp3!')</script>";
+    }else{
+        move_uploaded_file($arquivo['tmp_name'], $path) or die("Não foi possivel realizar o upload!");
+        echo "<script type='text/javascript'>alert('Evidado com sucesso!')</script>";
+    }
+
+} 
+
+if(isset($_POST['deletar'])){
+
+    $arquivo = $_FILES['file'];
+    $nomepastaupload = basename(__FILE__);
+    @$novonomepastaupload = pathinfo($nomepastaupload, PATHINFO_FILENAME);
+    $arquivoNovo = explode('.',$arquivo['name']);
+    $path = "../../../models/$novonomepastaupload/".$arquivo['name'];
+    $arquivoNome = $arquivo['name'];
+    
+    delTree("../../../models/$novonomepastaupload");
+    unlink("../../../views/pages/playlists/".basename(__FILE__));
+    header('Location: http://localhost:8000/mymp3/app/views/pages/playlist.php');
+    exit;
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php 
+            $nomedoarquivo = basename(__FILE__);
+            $nomedoarquivonovo = pathinfo($nomedoarquivo, PATHINFO_FILENAME);
+            echo $nomedoarquivonovo
+            ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <style>
+        *{
+            padding: 0%;
+            margin: 0%;
+        }
+        body{
+            font-family: 'Montserrat', Arial, sans-serif;
+            background-color: black;
+            padding: 0;
+            color: white;
+        }
+        .container{
+            z-index: 2;
+        }
+        svg{
+            position: fixed;
+            margin-top: 2%;
+        }
+        a{
+            color: white;
+            margin-top: 5%;
+            margin-left: 2%;   
+        }
+        a:hover{
+            transition: 0.5s;
+            color: #FF0099;
+        }
+        h1{
+            margin-top: 4%;
+            margin-bottom: 4%;
+        }
+        p{
+            width: 60%;
+            text-align: center;
+        }
+        .box{
+            color: white;
+            background-color: rgb(20,20,20);
+            border: 1px solid white;
+            margin-bottom: 5%;
+            padding: 2%;
+        }
+        audio{
+            width: 80%;
+            border-radius: 0%;
+        }
+        .upload{
+            padding: 5%;
+            width: 80%;
+            margin-top: 5%;
+            margin-bottom: 5%;
+            border: 1px white solid;
+            margin-left: 10%;
+        }
+        .enviar{
+            margin-top: 3%;
+            width: 30%;
+            color: white;
+            background-color: #FF0099;
+            border: 0px white;
+            border-radius: 30px;
+        }
+        .enviar:hover{
+            transition: 0.7s;
+            color: #FF0099;
+            background-color: white;
+        }
+        ::-webkit-scrollbar-track {
+        background-color: rgb(0, 0, 0);
+        }
+        ::-webkit-scrollbar {
+            width: 8px;
+            background: #F4F4F4;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: #dad7d7;
+        }
+    </style>
+</head>
+<body>
+    <a href="../playlist.php">
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"/></svg>
+    </a>
+    <center>
+        <div class="container">
+            <h1><strong>
+            <?php 
+            $nomedoarquivo = basename(__FILE__);
+            $nomedoarquivonovo = pathinfo($nomedoarquivo, PATHINFO_FILENAME);
+            echo $nomedoarquivonovo
+            ?></strong></h1>
+            <form action="" method="post" enctype="multipart/form-data" class="upload">
+                <center>
+                <label><h3>Insira um arquivo mp3 na playlist:</h3></label><br>
+                <input type="file" name="file" ><br>
+                <input type="submit" name="acao" class="enviar" value="ENVIAR"><br>
+                <input type="submit" name="deletar" class="enviar" value="DELETAR PASTA">
+                </center>
+            </form>
+
+            <?php 
+                @$arquivos = scandir("../../../models/$novonomepastaupload");
+                unset($arquivos[1]);
+                unset($arquivos[0]);
+
+                if(!isset($arquivos)){
+                    error_reporting(E_ERROR | E_PARSE);
+                }
+                if(!isset($novonomepastaupload)){
+                    error_reporting(E_ERROR | E_PARSE);
+                    unset($arquivos[2]);
+                    unset($arquivos[3]);
+                    unset($arquivos[4]);
+                }
+
+                foreach($arquivos as $arquivo) {
+                    echo "
+                    <center>
+                        <div class='box'>
+                        <label>$arquivo</label><br>
+                        <audio controls>
+                            <source src='../../../models/$novonomepastaupload/".$arquivo."' type='audio/ogg'>
+                            Your browser does not support the audio element.
+                        </audio>
+                        </div>
+                    </center>";
+                }
+    
+            ?>
+
+        </div>
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,59 @@
+<style>
+     .sidebar{
+            top: 0;
+            background-color: rgb(20,20,20);
+            position: fixed;
+            height: 100vh;
+            width: 7%;
+            z-index: 1;
+        }
+        h1{
+            width: 50%;
+        }
+        a{
+            color: white;
+        }
+        a:hover{
+            transition: 0.5s;
+            color: #FF0099;
+        }
+</style>
+</center>
+    <div class="sidebar">
+        <center>
+        <br>
+        <br>
+        <a href="../pages/home.php" alt="Home">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+            </svg><br>
+        </a>
+        <br>
+        <br>
+        <br>
+        <a href="../pages/banco.php" alt="Banco de musicas">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-server" viewBox="0 0 16 16"><path d="M1.333 2.667C1.333 1.194 4.318 0 8 0s6.667 1.194 6.667 2.667V4c0 1.473-2.985 2.667-6.667 2.667S1.333 5.473 1.333 4z"/><path d="M1.333 6.334v3C1.333 10.805 4.318 12 8 12s6.667-1.194 6.667-2.667V6.334a6.5 6.5 0 0 1-1.458.79C11.81 7.684 9.967 8 8 8s-3.809-.317-5.208-.876a6.5 6.5 0 0 1-1.458-.79z"/><path d="M14.667 11.668a6.5 6.5 0 0 1-1.458.789c-1.4.56-3.242.876-5.21.876-1.966 0-3.809-.316-5.208-.876a6.5 6.5 0 0 1-1.458-.79v1.666C1.333 14.806 4.318 16 8 16s6.667-1.194 6.667-2.667z"/>
+            </svg><br>
+        </a>
+        <br>
+        <br>
+        <br>
+        <a href="../pages/playlist.php" alt="Playlists">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-collection-play" viewBox="0 0 16 16"><path d="M2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1m2.765 5.576A.5.5 0 0 0 6 7v5a.5.5 0 0 0 .765.424l4-2.5a.5.5 0 0 0 0-.848z"/><path d="M1.5 14.5A1.5 1.5 0 0 1 0 13V6a1.5 1.5 0 0 1 1.5-1.5h13A1.5 1.5 0 0 1 16 6v7a1.5 1.5 0 0 1-1.5 1.5zm13-1a.5.5 0 0 0 .5-.5V6a.5.5 0 0 0-.5-.5h-13A.5.5 0 0 0 1 6v7a.5.5 0 0 0 .5.5z"/>
+            </svg><br>
+        </a>
+        <br>
+        <br>
+        <br>
+        <a href="../pages/upload.php" alt="Upload">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/><path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
+            </svg><br>
+        </a>
+        <br>
+        <br>
+        <br>
+        <a href="../pages/login.php" alt="Sair">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16"><path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.7 8.7 0 0 0-1.921-.306 7 7 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254l-.042-.028a.147.147 0 0 1 0-.252l.042-.028zM7.8 10.386q.103 0 .223.006c.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96z"/>
+            </svg>
+        </a>
+    </center>
+</div>
