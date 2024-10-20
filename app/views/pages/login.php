b41b4c6db834d555f170db0ba9d2dde544b58733
Gabriel Andrade <gandradecortez50@gmail.com>
2024-10-20 16:46:16 -0400

    Update login.php

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -16,11 +16,11 @@
             
             // Credenciais corretas, redireciona para a página home
             $_SESSION['name'] = $nome; // Armazena o nome de usuário na sessão
-            header('Location: http://localhost/mymp3/app/views/pages/home.php');
+            header('Location: http://localhost:8080/mymp3/app/views/pages/home.php');
             exit;
 
         }else{
-            header('Location: http://localhost/mymp3/app/views/pages/login.php');
+            header('Location: http://localhost:8080/mymp3/app/views/pages/login.php');
             exit;
         }
     }
@@ -102,7 +102,7 @@
         </form>
         
     </div>
-    <a href="http://localhost/mymp3/app/views/pages/home.php">Vizualizar como recrutador</a>
+    <a href="http://localhost:8080/mymp3/app/views/pages/home.php">Vizualizar como recrutador</a>
     </center>
 </body>
 </html>
