92eb802f992a8668080776c88a5df93e2434ecde
Gabriel Andrade <gandradecortez50@gmail.com>
2024-10-28 16:04:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+# Stage: Checkout
+echo "Checkout"
+rm -rf mymp3
+git clone https://github.com/andradesysadmin/mymp3.git
+
+# Stage: Build
+echo "Build"
+docker stop mymp3 || true
+docker rm mymp3 || true
+docker rmi mymp3 || true
+
+cd mymp3
+ls -la
+docker build -t mymp3 .
+cd ..
+
+# Stage: Deploy
+echo "Deploy"
+docker run -d --name mymp3 -p 8080:80 mymp3
