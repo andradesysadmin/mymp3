a7e1dbfead3b7abc14d123176e3711e0f89bcfb1
Gabriel Andrade <gandradecortez50@gmail.com>
2024-11-11 12:31:30 -0400

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
-# Armazena o gerenciador de pacotes da distro
 PACKAGE_MANAGER=""
-
-# Comando de instalação
 INSTALL_CMD=""
-
-# Configurações iniciais para a prosseguir com a instalção
 CONFIGS=""
 
 function install(){
-
-    eval $CONFIGS
-    eval $INSTALL_CMD
+    eval "$CONFIGS"
+    eval "$INSTALL_CMD"
 
     sudo systemctl start docker
     sudo systemctl enable docker
     sudo usermod -aG docker $USER
 }
 
-#Verifica se o docker ja esta instalado
-
-if command -v docker --version > /dev/null; then
-
+if command -v docker >/dev/null 2>&1; then
     echo "Docker já está instalado"
-    exit 0;
-
+    exit 0
 fi
 
 if command -v apt >/dev/null; then
-
     PACKAGE_MANAGER="apt"
-    INSTALL_CMD="sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+    INSTALL_CMD="sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
     CONFIGS="sudo apt-get update
-            sudo apt-get install ca-certificates curl
+            sudo apt-get install -y ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -42,33 +31,27 @@ if command -v apt >/dev/null; then
             sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
             sudo apt-get update"
 
-
 elif command -v dnf >/dev/null; then
-
     PACKAGE_MANAGER="dnf"
     INSTALL_CMD="sudo dnf install -y docker-ce docker-ce-cli containerd.io"
     CONFIGS="sudo dnf install -y dnf-plugins-core && sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo"
 
 elif command -v yum >/dev/null; then
-
     PACKAGE_MANAGER="yum"
     INSTALL_CMD="sudo yum install -y docker-ce docker-ce-cli containerd.io"
     CONFIGS="sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
 
 elif command -v pacman >/dev/null; then
-
     PACKAGE_MANAGER="pacman"
     INSTALL_CMD="sudo pacman -S --noconfirm docker"
     CONFIGS="sudo pacman -Sy"
 
 elif command -v zypper >/dev/null; then
-
     PACKAGE_MANAGER="zypper"
     INSTALL_CMD="sudo zypper install -y docker"
     CONFIGS="sudo zypper refresh"
 
 else
-
     PACKAGE_MANAGER="Gerenciador de pacotes desconhecido!"
     echo $PACKAGE_MANAGER
     exit 1
